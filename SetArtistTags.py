067a4b56f9c188a0d961587e892ac29b14df23ab
Kieran Meekan <Kebiled@users.noreply.github.com>
2020-03-04 09:37:18 +1300

    Changed to minimise code
    Some work still needed to finish

--- SetArtistTags.py
+++ SetArtistTags.py
@@ -6,57 +6,41 @@ Author: Kieran Meekan
 Date: 20200303
 """
 
-# TO DO:
-# - Add a check for tags already existing
-# - Currently will not work on songs which have a '-' character in the title (i.e. remixes)
-#       - Need to check how the names are done by deezloader to ensure i can sort 
-
 import os
 import eyeD3
 
-errorCount = 0
-errorList = []
-paths = [
-    'H O U S E/',
-    'dnb tunes/',
-    'Spin me some 70s/'
-]
-
 def get_artist_name(fileName):
     """ fileName format "trackNumber - artist - song.mp3" """
-    if  len(fileName.split(" - ")) == 3:
-        artistName = fileName.split(" - ")[1]
-        return artistName.encode('unicode', 'ignore')
-    else:
-        print("Error: " + fileName + " has an incorrect number of '-' characters, unable to process")
-        errorList += [fileName]
-        errorCount += 1 
-        return "".encode('unicode', 'ignore') # in python 3 all text is stored as unicode, this may not be required
+    artistName = fileName.split(" - ")[1]
+    return artistName
+
+def check_for_tag(path, fileName):
+    tag = eyeD3.Tag()
+    tag.link(path + fileName)
+    print(tag.getArtist())
+    # this needs to be finished for the code to work
+    return True
 
 def set_artist_name(path, fileName):
-    with open(path + fileName, 'r+') as file:
-        if eyeD3.isMp3File(file):
-            audioFile = eyeD3.Mp3AudioFile(file)
-            tag = audioFile.getTag(audioFile)
-            tag.setArtist(get_artist_name(fileName))
-            tag.update()
-            print(fileName + " - tag updated successfully")
-        else:
-            print("Error: " + fileName + " is not an mp3 file")
-            errorList += [fileName]
-            errorCount += 1 
+    tag = eyeD3.Tag()
+    tag.link(path + fileName)
+    tag.setArtist(get_artist_name(fileName))
+    tag.update()
+    print(fileName + " - tag updated successfully")
 
 def main():
+    paths = os.listdir()
     for path in paths:
-        print("Processing files from " + path)
-        files = os.listdir(path)
-        for fileName in files:
-            set_artist_name(path, fileName)
-    if errorCount == 0:
-        print("Finished without error, all tags updated")
-    else:
-        print("Finished. Errors with the following files:")
-        for fileName in errorList:
-            print(fileName)
+        if '.' not in path:
+            path += '/'
+            print("Processing files from " + path)
+            files = os.listdir(path)
+            for fileName in files:
+                if not check_for_tag(path, fileName):
+                    set_artist_name(path, fileName)
 
 main()
+
+
+
+
